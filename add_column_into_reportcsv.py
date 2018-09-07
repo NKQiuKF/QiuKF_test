8835dc700dc2b7f8ad451715a99590117e2f1067
NKQiuKF <qiukefan@mail.nankai.edu.cn>
2018-09-07 11:00:01 +0800

    add operate code used on 150

--- add_column_into_reportcsv.py
+++ add_column_into_reportcsv.py
@@ -13,11 +13,73 @@ import os
 import pandas as pd
 import time
 import sys
+import re
 
 SAMPLES_PATH='/data/malware/'
 #SAMPLES_PATH='/data/benign/'
 
-def add_columns(first_dir):
+#detect sample file by sha256
+# return is boolean
+
+def is_sample(file_name)
+  detect_pattern='[0123456789abcdef]{64}$'
+  return bool(re.match(detect_pattern, file_name))
+
+def file_cmd_ret(abs_path):
+	file_cmd=os.popen("file "+abs_path)
+	file_ret=file_cmd.read()
+	processed_ret=file_ret[file_ret.index(':')+2:file_ret.index('\n')]
+	return processed_ret
+	#file_cmd.read() return e.g. 
+	#'3edff642fcd311d66c6f400f924700d606bb1cd5de1de3565ba99fc83b207636: Java archive data (JAR)\n'
+
+def add_columns_in_150(first_dir):
+  
+
+  print 'Run task %s (%s)...' % (first_dir, os.getpid())
+  child_dir=make_file_dir(first_dir)
+  for each_dir in child_dir:
+    #process file results first
+    #filter files like .data,.xml
+    samples_files=os.listdir(SAMPLES_PATH+each_dir)
+    for each_file in all_files:
+      if not is_sample(each_file):
+        samples_files.remove(each_file)
+    file_ret_list=[]
+    for each_sample in samples_files:
+      try:
+        sample_file_ret=file_cmd_ret(SAMPLES_PATH+each_dir+each_sample)
+      except Exception,e:
+        print e
+        sample_file_ret=' '
+      file_ret_list.append(sample_file_ret)
+    file_ret_pd=pd.DataFrame({'sha256':samples_files,'file':file_ret_list})
+    # test
+    print file_ret_pd
+    
+    try:
+      vt_report_pd=pd.read_csv(SAMPLES_PATH+each_dir+'vt_report.csv')
+      #use in server:150
+      #reports_pd=pd.read_csv(SAMPLES_PATH+each_dir+'vt_report.csv')
+    except Exception,e:
+      print e,' in ',SAMPLES_PATH+each_dir+'vt_report.csv'
+      continue
+    vt_report_pd['file_name']=' '
+    vt_report_pd['file_size']=' '
+    #replace it by scan_date
+    #reports_pd['last_analysis_time']=' '
+    vt_report_pd['time_stamp']=' '
+
+    #concat
+    reports_pd=pd.merge(vt_report_pd,file_ret_pd,on=['sha256'],how='outer')
+    reports_pd=reports_pd.fillna(' ')
+    reports_pd.drop_duplicates('sha256','first',inplace=True)
+    #write
+    reports_pd.to_csv(SAMPLES_PATH+each_dir+'reports.csv',index=False)
+    #delete vt_report.csv
+    #os.popen('rm -f '+SAMPLES_PATH+each_dir+'vt_report.csv')
+
+def add_columns_in_151(first_dir):
 
   print 'Run task %s (%s)...' % (first_dir, os.getpid())
   child_dir=make_file_dir(first_dir)
@@ -31,9 +93,9 @@ def add_columns(first_dir):
       continue
     reports_pd['file_name']=' '
     reports_pd['file_size']=' '
-    reports_pd['last_analysis_time']=' '
-    #
-    reports_pd['time']=' '
+    #replace it by scan_date
+    #reports_pd['last_analysis_time']=' '
+    reports_pd['time_stamp']=' '
     #
     reports_pd=reports_pd.fillna(' ')
     #
