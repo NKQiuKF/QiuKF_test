21c6654ca7fe5538709890f1cc3e0658b9b758d2
NKQiuKF <qiukefan@mail.nankai.edu.cn>
2018-09-09 20:24:46 +0800

    delete the lines in reorts.csv with .data and .xml

---
+++ clean_reports.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#Nankai University Information Security
+#QiuKF 1055419050@qq.com
+#add file_name,file_size,last_analysis_time  columns into reports.csv
+#if there is no file_name for a sample,replace its name by sha256
+#add time
+#Wait Tian's time.csv
+
+from multiprocessing import Process,Pool
+import os
+import pandas as pd
+import time
+import sys
+import re
+
+#clean the reports.csv in every sub dirctory 
+#rename reports.csv  as reports_future.csv
+#create reports.csv only contains samples info with its label
+
+MALWARE_PATH='/data/malware/'
+BENIGN_PATH='/data/benign/'
+
+
+def clean_reports(first_dir):
+
+  print 'Run task %s (%s)...' % (first_dir, os.getpid())
+  child_dir=make_file_dir(first_dir)
+  for each_dir in child_dir:
+    try:
+      reports_pd=pd.read_csv(SAMPLES_PATH+each_dir+'reports.csv')
+      #use in server:150
+      #reports_pd=pd.read_csv(SAMPLES_PATH+each_dir+'vt_report.csv')
+    except Exception,e:
+
+      print e,' in ',SAMPLES_PATH+each_dir+'reports.csv'
+      continue
+    #clean lines which sha256 column is't sha256
+    filter_pd=reports_pd[~reports_pd.sha256.str.contains('\.')]
+    filter_pd.to_csv(SAMPLES_PATH+each_dir+'reports_future.csv',index=False)
+
+    tjw_need=filter_pd[~filter_pd['scan_date']==' ']
+    tjw_need.to_csv(SAMPLES_PATH+each_dir+'reports.csv',index=False)
+
+def make_file_dir(first):
+  ret=[]
+  chr_list=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+  tmp=''
+  for second in chr_list:
+    two='/'+second
+    for third in chr_list:
+      three=two+'/'+third+'/'
+      ret.append(first+three)
+  return ret
+  
+def main():
+  print('Parent process %s.' %os.getpid())
+  first_dic=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+  p=Pool(16)
+  for each in first_dic:
+    p.apply_async(clean_reports,args=(each,))
+  p.close()
+  p.join()
+
+
+if __name__=='__main__':
+  main()
+
