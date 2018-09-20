0120604cff813ce1698f91691da9d2ea43d21c91
NKQiuKF <qiukefan@mail.nankai.edu.cn>
2018-09-20 14:54:25 +0800

    .

--- clean_reports.py
+++ clean_reports.py
@@ -25,6 +25,7 @@ def clean_reports(first_dir):
   for each_dir in child_dir:
     try:
       reports_pd=pd.read_csv(SAMPLES_PATH+each_dir+'reports.csv')
+      reports_pd.drop_duplicates('sha256','last',inplace=True)
       #use in server:150
       #reports_pd=pd.read_csv(SAMPLES_PATH+each_dir+'vt_report.csv')
     except Exception,e:
@@ -32,7 +33,7 @@ def clean_reports(first_dir):
       print e,' in ',SAMPLES_PATH+each_dir+'reports.csv'
       continue
     #clean lines which sha256 column is't sha256
-    filter_pd=reports_pd[~reports_pd.md5.str.contains('apk')]
+    #filter_pd=reports_pd[~reports_pd.md5.str.contains('apk')]
     filter_pd.to_csv(SAMPLES_PATH+each_dir+'reports.csv',index=False)
     print SAMPLES_PATH+each_dir,' completed'
 
